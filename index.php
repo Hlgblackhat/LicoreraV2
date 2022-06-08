279718d6dfc10f226d966dfa6786b619b2013e5f
Haider Lopez <haiderlg@outlook.com>
2022-06-07 22:16:11 -0500

    colocando background del login color blanco

--- index.php
+++ index.php
@@ -48,25 +48,22 @@ if (!empty($_SESSION['active'])) {
   <meta name="author" content="">
 
   <title>Licores Cismar</title>
-
-  <!-- Custom fonts for this template-->
+  <!--link href="sistema/css/estilos.csss" rel="stylesheet"-->
+  <!-- Custom fonts-->
   <link href="sistema/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <!-- Custom styles for this template-->
+  <!-- Custom styles-->
   <link href="sistema/css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 
-<body class="bg-gradient-primary">
-<!---->
+<body class="body">
   <div class="container">
-    <!-- Outer Row -->
     <div class="row justify-content-center">
 
       <div class="col-xl-10 col-lg-12 col-md-9">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
+
             <div class="row">
               <div class="col-lg-6 d-none d-lg-block bg-login-image">
                 <img src="sistema/img/logo.jpeg" class="img-thumbnail">
@@ -86,9 +83,9 @@ if (!empty($_SESSION['active'])) {
                       <input type="password" class="form-control" placeholder="Contraseña" name="clave">
                     </div>
                     <input type="submit" value="Iniciar" class="btn btn-primary">
-                    <hr>
+
                   </form>
-                  <hr>
+
                 </div>
               </div>
             </div>
